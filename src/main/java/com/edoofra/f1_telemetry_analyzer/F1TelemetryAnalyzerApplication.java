5f1d410860db758fb5950f640ca35364f7291c57
edoofra <edoardo.fra.ef@gmail.com>
2025-08-17 10:17:56 +0200

    init spring boot project

---
+++ src/main/java/com/edoofra/f1_telemetry_analyzer/F1TelemetryAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.edoofra.f1_telemetry_analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F1TelemetryAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(F1TelemetryAnalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edoofra/f1_telemetry_analyzer/F1TelemetryAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edoofra.f1_telemetry_analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class F1TelemetryAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
